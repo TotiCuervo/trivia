972607e272dc1de041b487721f85b8c7dc10ad70
Toti Cuervo <toticuervo@Totis-MBP.attlocal.net>
2019-10-31 03:20:29 -0500

    pushing time store

--- resources/js/components/Question/QuestionTimer.vue
+++ resources/js/components/Question/QuestionTimer.vue
@@ -12,8 +12,8 @@
             }
         },
         mounted() {
-            // this.$store.commit('play/UPDATE_TIMER', 99);
-            this.timer = this.rounds[this.playRoundPosition].time;
+            this.$store.commit('play/UPDATE_TIMER', this.rounds[this.playRoundPosition].time);
+            // this.timer = this.rounds[this.playRoundPosition].time;
             this.startTimer();
         },
         methods: {
